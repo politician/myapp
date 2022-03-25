b96504f65e63522a9591ace20689de02c2093187
Romain Barissat <romain-noreply@barissat.com>
2022-03-25 21:49:57 +0700

    Do not send traces on observability endpoints
    Signed-off-by: Romain Barissat <romain-noreply@barissat.com>

--- src/observability/tracer.js
+++ src/observability/tracer.js
@@ -11,7 +11,11 @@ const sdk = new opentelemetry.NodeSDK({
     [SemanticResourceAttributes.SERVICE_NAME]: process.env.SERVICE_NAME || "myapp",
   }),
   instrumentations: [
-    new HttpInstrumentation(),
+    new HttpInstrumentation({
+      // Soon replaced by ignoreIncomingRequestHook
+      // https://github.com/open-telemetry/opentelemetry-js/blob/549bd5b9f82ab4f580fcfecb5416b5b645ed32ee/experimental/packages/opentelemetry-instrumentation-http/test/functionals/https-enable.test.ts#L195-L197
+      ignoreIncomingPaths: ["/metrics", "/livez", "/readyz"],
+    }),
     new ExpressInstrumentation(),
     new PinoInstrumentation(),
   ],
